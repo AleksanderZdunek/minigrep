afb83f1c1e8e807cee7ffa41ce30b4bc1f4569bf
Aleksander <halekun@gmail.com>
2020-04-25 21:30:15 +0200

    chapter 12.4

---
+++ src/main.rs
@@ -0,0 +1,23 @@
+//use std::env;
+//use std::process;
+
+//use minigrep::Config;
+
+fn main() {
+    println!("Rust book chapter 12 minigrep");
+
+    let args: Vec<String> = std::env::args().collect();
+    //println!("{:?}", args);
+    let config = minigrep::Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        std::process::exit(1);
+    });
+
+    //println!("Searching for {}", config.query);
+    //println!("In file {}", config.filename);
+
+    if let Err(e) = minigrep::run(config) {
+        println!("Application error: {}", e);
+        std::process::exit(1);
+    }
+}
