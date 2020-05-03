7c0d3a1313d23419efc44fcb63add76eaabb875e
Aleksander <halekun@gmail.com>
2020-05-03 17:23:22 +0200

    chapter 12.6

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ fn main() {
     let args: Vec<String> = std::env::args().collect();
     //println!("{:?}", args);
     let config = minigrep::Config::new(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {}", err);
+        eprintln!("Problem parsing arguments: {}", err);
         std::process::exit(1);
     });
 
@@ -17,7 +17,7 @@ fn main() {
     //println!("In file {}", config.filename);
 
     if let Err(e) = minigrep::run(config) {
-        println!("Application error: {}", e);
+        eprintln!("Application error: {}", e);
         std::process::exit(1);
     }
 }
